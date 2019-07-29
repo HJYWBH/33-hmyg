4d14dac21456ef07c19d95f50d09b1906e14875a
uksir <yeah126139163@163.com>
2019-07-29 17:53:24 +0800

    分类-左右两侧容器

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,26 @@
-// pages/category/index.js
+import { request } from "../../request/index.js";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 左侧的菜单
+    leftMenuList:[],
+    // 右侧的商品数组
+    rightGoodsList:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(){
+    this.getCategoryList();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取分类页面的接口数据
+  getCategoryList(){
+    request({url:"/categories"})
+    .then(result=>{
+      // 左侧菜单要的数据
+      const leftMenuList=result.map(v=>({cat_id:v.cat_id,cat_name:v.cat_name}));
+      // 大家电   result[0].children
+      const rightGoodsList=result[0].children;
+      this.setData({
+        leftMenuList,
+        rightGoodsList
+      })
+    })
   }
 })
