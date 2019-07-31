2f84ef43f8a508c8d9cdd86835414e5da55f7248
uksir <yeah126139163@163.com>
2019-07-31 17:19:19 +0800

    商品列表-下拉刷新

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -15,6 +15,15 @@
       1 页码 pagenum++ 
       2 直接发送请求获取数据
       3 数据回来后 对商品数组进行拼接 而不是全部替换
+3 下拉刷新
+  1 用户往下滑动页面的时候 开启下拉刷新效果  
+    页面的json文件 加入一个允许下拉属性
+  2 找到下拉刷新事件  onPullDownRefresh
+  3 重置
+    1 重置页码 pagenum=1;
+    2 重置商品数组 data中的商品数组 进行重置 []
+    3 重新发送请求 
+    4 数据请求回来后 手动关闭 页面的下拉刷新效果 
  */
 import { request } from "../../request/index.js";
 Page({
@@ -39,7 +48,7 @@ Page({
     pagesize: 10
   },
   // 总页数
-  TotalPages:1,
+  TotalPages: 1,
 
   // 页面开始加载就触发
   // 它的形参可以获取到url上的参数
@@ -54,12 +63,17 @@ Page({
   getGoodsList() {
     request({ url: "/goods/search", data: this.QueryParams })
       .then(result => {
-      //  计算总页数
-      this.TotalPages=Math.ceil(result.total/this.QueryParams.pagesize);
+        //  计算总页数
+        this.TotalPages = Math.ceil(result.total / this.QueryParams.pagesize);
         this.setData({
           // 要拼接数组
-          goodsList: [...this.data.goodsList,...result.goods]
+          goodsList: [...this.data.goodsList, ...result.goods]
         })
+
+        // 关闭页面的下拉刷新效果
+        // 1 有些框架 一些组件没有开启 就关闭可能  会报错
+        // 2 有些框架 一些组件没有开启 就关闭可能  不会报错！！
+        wx.stopPullDownRefresh();
       })
   },
   // 子组件触发的事件
@@ -74,15 +88,30 @@ Page({
   },
   // 页面上拉 滚动条触底事件
   onReachBottom() {
-  //  1 判断有没有下一页数据
-  if(this.QueryParams.pagenum>=this.TotalPages){
-    // 没有下一页数据
-    console.log("没有下一页数据");
-  }else{
-    // 还存在下一页数据
-    // console.log("还存在下一页数据");
-    this.QueryParams.pagenum++;
+    //  1 判断有没有下一页数据
+    if (this.QueryParams.pagenum >= this.TotalPages) {
+      // 没有下一页数据
+      // console.log("没有下一页数据");
+      // Toast 会自动显示的提示框
+      wx.showToast({
+        title: '没有下一页数据了',
+        icon: "none"
+      });
+
+    } else {
+      // 还存在下一页数据
+      // console.log("还存在下一页数据");
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
+  },
+  // 用户下拉页面事件
+  onPullDownRefresh(){  
+    this.QueryParams.pagenum=1;
+    this.setData({
+      goodsList:[]
+    })
     this.getGoodsList();
-  }
+
   }
 })
