08e69ba81ee5128eca9168a763a692d73ab27c31
uksir <yeah126139163@163.com>
2019-07-31 15:55:33 +0800

    商品列表-获取接口数据

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,38 @@
-// pages/goods_list/index.js
+import { request } from "../../request/index.js";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 页面要渲染的商品数组
+    goodsList:[]
+  },
+  // 接口用的参数
+  QueryParams: {
+    // 搜索的关键字 
+    query: "",
+    // 分类id
+    cid: "",
+    // 页码
+    pagenum: 1,
+    // 页容量
+    pagesize: 10
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  // 页面开始加载就触发
+  // 它的形参可以获取到url上的参数
+  onLoad(options) {
+    // console.log(options);
 
+    this.QueryParams.cid=options.cid;
+    this.getGoodsList();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取商品列表数据
+  getGoodsList() {
+    request({ url: "/goods/search" ,data:this.QueryParams})
+      .then(result => {
+        // console.log(result);
+        this.setData({
+          goodsList:result.goods
+        })
+      })
   }
 })
